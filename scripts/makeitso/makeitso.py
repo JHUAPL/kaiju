78170c7b407d7ff6eb676b20b5422de272fec340
cookieenick <nikhilr@ucar.edu>
2025-08-08 08:26:10 -0600

    makeitso updates for dtcon

--- scripts/makeitso/makeitso.py
+++ scripts/makeitso/makeitso.py
@@ -117,7 +117,39 @@ def create_command_line_parser():
     )
     return parser
 
+def update_loaded_options(options):
+    """Update a loaded set of run options to account for format changes.
 
+    Update a loaded set of run options to account for format changes.
+
+    Parameters
+    ----------
+    options : dict
+        Dictionary of program options, each entry maps str to str.
+
+    Returns
+    -------
+    options : dict
+        Updated dictionary of program options.
+
+    Raises
+    ------
+    None
+    """
+    # <HACK>
+    # If the tsOut attribute is found, rename it to dtCon.
+    DEFAULT_GEO_DTCON = "300"  # Seconds
+    if "tsOut" in options["voltron"]["output"]:
+        print("WARNING: Replacing obsolete parameter tsOut with default dtCon"
+              f" value of {DEFAULT_GEO_DTCON} seconds!")
+        options["voltron"]["output"]["dtCon"] = DEFAULT_GEO_DTCON
+        del options["voltron"]["output"]["tsOut"]
+    # </HACK>
+
+    # Return the updated options.
+    return options
+
+    
 def load_option_descriptions(path: str = OPTION_DESCRIPTIONS_FILE):
     """Load the option descriptions and update as needed.
 
@@ -1240,6 +1272,7 @@ def makeitso(args: dict = None):
             print(f"Reading run options from {options_path}.")
         with open(options_path, "r", encoding="utf-8") as f:
             options = json.load(f)
+        options = update_loaded_options(options)
     else:
         # Prompt the user for the run options.
         options = prompt_user_for_run_options(option_descriptions, args)
